f8e4671bebfd021c63b3f920b586a0d78229405e
evoronin <elnvoronin@gmail.com>
2025-03-28 16:28:49 +0100

    mariadb created

--- srcs/requirements/mariadb/tools/setup.sql
+++ srcs/requirements/mariadb/tools/setup.sql
@@ -1,13 +1,13 @@
 -- this is an SQL script 
 
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
 
 -- Create the user if it doesn’t already exist
-CREATE USER IF NOT EXISTS ${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 
 -- Grant all privileges on the database to the user
 
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
 
 -- Ensure privileges take effect immediately
 
