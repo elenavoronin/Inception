9e8b56cc37afd97fbbcb7cd0c65925b88830091b
evoronin <elnvoronin@gmail.com>
2025-03-28 15:06:50 +0100

    updates

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -14,13 +14,19 @@ echo "Starting Wordpress setup..."
 if [ ! -f /var/www/html/wp-config.php ] &&
     [ ! -f /var/www/html/index.php ];
     then
-    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} create wordpress
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" create wordpress
 
-    wp config create --path=/var/www/html --dbname=wordpress --dbuser=root --dbpass="$MYSQL_ROOT_PASSWORD"
+    wp config create --path=/var/www/html --dbname=wordpress --dbuser=root --dbpass="${MYSQL_ROOT_PASSWORD}"
 
-    wp core install --path=/var/www/html --url="http://localhost" --title="My WordPress Site" --admin_user=admin --admin_password=changeme --admin_email=admin@example.com
+    wp core install --path=/var/www/html --url="http://localhost" --
+    title="My WordPress Site" --admin_user="${WORDPRESS_DB_ADMIN}" --admin_password="${WORDPRESS_DB_ADMIN_PASSWORD}" --
+    admin_email="${WORDPRESS_DB_ADMIN_EMAIL}"
 
+    wp user create "${WORDPRESS_DB_USER}" "${WORDPRESS_DB_USER_EMAIL}" --role=editor --path=/var/www/html
+fi
 
+# Keep the service running in the background
+/usr/sbin/php-fpm8.2 -F
 
 
 
