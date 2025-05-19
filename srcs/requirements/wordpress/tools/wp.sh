efcf616061a29dde39b3eb2cbffec5879991df8c
Lena <elnvoronin@gmail.com>
2025-05-19 14:39:14 +0200

    mariadb and wp setup

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# Exit on error
-set -e
-
-# load .env variables
-set -o allexport
-source /docker-entrypoint-initdb.d/.env
-set +o allexport
 
 echo "Starting Wordpress setup..."
 
 # check for any config files
-if [ ! -f /var/www/html/wp-config.php ] &&
-    [ ! -f /var/www/html/index.php ];
+if [ ! -f /var/www/html/wp-config.php ];
     then
-    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" create wordpress
-
-    wp config create --path=/var/www/html --dbname=wordpress --dbuser=root --dbpass="${MYSQL_ROOT_PASSWORD}"
-
-    wp core install --path=/var/www/html --url="http://localhost" --
-    title="My WordPress Site" --admin_user="${WORDPRESS_DB_ADMIN}" --admin_password="${WORDPRESS_DB_ADMIN_PASSWORD}" --
-    admin_email="${WORDPRESS_DB_ADMIN_EMAIL}"
-
-    wp user create "${WORDPRESS_DB_USER}" "${WORDPRESS_DB_USER_EMAIL}" --role=editor --path=/var/www/html
+    sleep 5
+    wp core download --allow-root
+    wp config create --path=/var/www/html --dbhost="${WORDPRESS_DB_HOST}":3306 --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --allow-root
+    wp core install --path=/var/www/html --url="${DOMAIN_NAME}" \
+    --title="My WordPress Site" --admin_user="${WORDPRESS_DB_ADMIN}" \
+    --admin_password="${WORDPRESS_DB_ADMIN_PASSWORD}" \
+    --admin_email="${WORDPRESS_DB_ADMIN_EMAIL}" --allow-root
+    wp user create "${WORDPRESS_DB_USER}" "${WORDPRESS_DB_USER_EMAIL}" --user_pass="${WORDPRESS_DB_PASSWORD}" --role=editor --path=/var/www/html --allow-root
 fi
 
 # Keep the service running in the background
-/usr/sbin/php-fpm8.2 -F
+/usr/sbin/php-fpm7.4 -F
 
 
 
